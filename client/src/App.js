dcde326d1a0af2c2ed4c2a8f4f44c0a515c7fadf
ajmeraaditya02 <ajmeraaditya05@gmail.com>
2024-02-14 12:06:57 +0530

    Initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import Home from "./Pages/Home/Home.js"
 import Profile from "./Pages/Profile/Profile.js"
+import About from "./Pages/About/About.js"
 import {BrowserRouter as Router,Route,Routes as Switch} from "react-router-dom" 
 
 function App() {
   return (
     <div>
-      {/* <Router>
+      { 
+      <Router>
         <Switch>
           <Route path="/" element={<Home/>}/>
           <Route path="/profile" element={<Profile/>}/>
+          <Route path="/about" element={<About/>}/>
         </Switch>
-      </Router> */}
-      <Home/>
+      </Router>
+      }
     </div>
   );
 }
---
+++ client/src/Pages/About/About.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p1>This is an initial about page of my project</p1>
+        </div>
+    );
+}
+
+export default About;
