cfba5c1189bd814ac9ed5570d24c26d329daef18
ajmeraaditya02 <ajmeraaditya05@gmail.com>
2024-02-14 16:00:25 +0530

    New

--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -1,12 +1,45 @@
-import react from 'react';
+import React from 'react';
 
 function About() {
-    return (
-        <div>
-            <h1>About Page</h1>
-            <p1>This is an initial about page of my project</p1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>About Exam Management System</h1>
+      <p>Welcome to the Exam Management System of The LNMIIT. This system provides a comprehensive solution for managing exams efficiently.</p>
+      
+      <h2>Features</h2>
+      <ul>
+        <li>Creation and scheduling of exams</li>
+        <li>Creating a Seating Arrangement plan for each exam.</li>
+        <li>Secure user authentication and authorization</li>
+      </ul>
+
+      <h2>Mission and Vision</h2>
+      <p>Our mission is to simplify the exam management process for educators, administrators and students. We envision a future where exams are administered seamlessly, enabling better assessment and learning outcomes.</p>
+
+      <h2>Team</h2>
+      <ul>
+        <li>Aditya Ajmera</li>
+        <ul>
+            <li>Email :</li>
+            <li>Mobile No :</li>
+        </ul>
+        <li>Aditya Garg</li>
+        <ul>
+            <li>Email :</li>
+            <li>Mobile No :</li>
+        </ul>
+      </ul>
+
+      <h2>Contact Information</h2>
+      <p>If you have any questions, feedback, or support inquiries, please contact us at <a href="xyz.com">info@example.com</a>.</p>
+
+      <h2>Acknowledgments</h2>
+      <p>We would like to thank the ......</p>
+
+      <h2>Feedback and Suggestions</h2>
+      <p>We value your feedback! Please share your suggestions and ideas with us <a href="/feedback">here</a>.</p>
+    </div>
+  );
 }
 
 export default About;
