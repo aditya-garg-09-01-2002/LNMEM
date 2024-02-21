8687fd064d963f502c6439c1e4cd4be2bc9fead6
ajmeraaditya02 <ajmeraaditya05@gmail.com>
2024-02-21 20:37:03 +0530

    neew

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,10 +1,31 @@
 import React from "react"
-import Navbar from "../../components/button/NavBar/Navbar"
+import Navbar from "../../components/Navigationbar/Navbar"
+import "./Home.css"
+import logo from "./logo5.png"
 
 export default function Home(){
-    return (
-        <div>
+    return(
+        <div className="M" style={{
+            overflowY:"hidden"
+        }}>
             <Navbar />
+            <img src={logo} style={{
+                position:"fixed",
+                height:"100dvh",
+                width:"100dvw",
+                zIndex:"-1",
+                top:20
+            }}/>
+            <div className="container">
+                <div className="heading">
+                    <h1>
+                        Welcome to the Exam Management System of The LNMIIT!
+                    </h1>
+                </div>
+            </div>
+            {/* <div className="image">
+                <img className = "i1" src={logo} alt="Adi" />
+            </div> */}
         </div>
     )
 }
